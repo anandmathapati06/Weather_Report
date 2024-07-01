f868b0fab4e624ec5f72cec40109212cd5c839ae
AnandMathapati <0606dnana@gmail.com>
2024-07-02 01:00:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("hello");
+
+let data
+console.log(cityname.value);
+
+    
+
+
+
+
+
+
+
+
+
+
+async function  apicall(){
+    let place = (cityname.value == "") ? "india": cityname.value
+    const url =`http://api.weatherapi.com/v1/current.json?q=${place}`
+    let res = await fetch(url,{
+        method : "GET",
+    headers :
+    { 
+        "key" : "9c0b299034a343f79af90631241804",
+        
+    },
+     
+    
+})
+    let data = await res.json()
+    console.log(data);
+    temp.innerHTML = data.current.temp_c
+    tempf.innerHTML = data.current.temp_f
+    wind.innerHTML = data.current.wind_kph+"KPH"
+    wind_direction.innerHTML= data.current.wind_dir 
+    country.innerHTML= data.location.name
+    last_updated.innerHTML = data.current.last_updated
+    return data
+}
+
+async function use(){
+    data = await apicall()
+}
+use()
+
+window.onbeforeunload = apicall()
